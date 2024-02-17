ed7d7fa4c5137a3cad509f6b1d4b5c864b61fc0a
Eslam ahmed <eslamahmedwork@gmail.com>
2024-02-17 02:38:12 +0200

    db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,187 +14,183 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory()->create([
-        //     'name' => 'admin',
-        //     'email' => 'admin@gmail.com',
-        //     'password' => Hash::make("admin@gmail.com"),
-        //     "active" => "1"
-        // ]);
+
+        \App\Models\role::create([
+            'name' => 'كل الصلاحيات',
+            "permissions" => '["users","roles","home","about","services","courses","projects","faqs","settings","pages","emails","messages"]'
+        ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make("admin@gmail.com"),
+            "active" => "1",
+            "role_id" => 1,
+        ]);
 
 
-        //     $columns = [
-        //         // header
-        //         [
-        //             "key" => "header_img",
-        //             "value" => ""
-        //         ],
-        //         [
-        //             "key" => "header_title",
-        //             "value" => ""
-        //         ],
-        //         [
-        //             "key" => "header_dis",
-        //             "value" => ""
-        //         ],
-        //         // about
-        //         [
-        //             "key" => "about_dis",
-        //             "value" => ""
-        //         ],
-        //         [
-        //             "key" => "our_services_dis",
-        //             "value" => ""
-        //         ],
-        //         // sp
-        //         [
-        //             "key" => "sp_img",
-        //             "value" => ""
-        //         ],
-        //         [
-        //             "key" => "sp",
-        //             "value" => ""
-        //         ],
-        //         // saying
-        //         [
-        //             "key" => "saying_img",
-        //             "value" => ""
-        //         ],
-        //         [
-        //             "key" => "saying_dis",
-        //             "value" => ""
-        //         ],
-        //         // call_us
-        //         [
-        //             "key" => "call_dis",
-        //             "value" => ""
-        //         ],
-        //         [
-        //             "key" => "call_adress",
-        //             "value" => ""
-        //         ],
-        //         [
-        //             "key" => "call_title",
-        //             "value" => ""
-        //         ],
-        //         [
-        //             "key" => "call_email",
-        //             "value" => ""
-        //         ],
-        //         [
-        //             "key" => "call_phone",
-        //             "value" => ""
-        //         ],
 
-        //     ];
-        //     DB::table('homes')->insert($columns);
+        $columns = [
+            // header
+            [
+                "key" => "header_img",
+                "value" => ""
+            ],
+            [
+                "key" => "header_title",
+                "value" => ""
+            ],
+            [
+                "key" => "header_dis",
+                "value" => ""
+            ],
+            // about
+            [
+                "key" => "about_dis",
+                "value" => ""
+            ],
+            [
+                "key" => "our_services_dis",
+                "value" => ""
+            ],
+            // sp
+            [
+                "key" => "sp_img",
+                "value" => ""
+            ],
+            [
+                "key" => "sp",
+                "value" => ""
+            ],
+            // saying
+            [
+                "key" => "saying_img",
+                "value" => ""
+            ],
+            [
+                "key" => "saying_dis",
+                "value" => ""
+            ],
+            // call_us
+            [
+                "key" => "call_dis",
+                "value" => ""
+            ],
+            [
+                "key" => "call_adress",
+                "value" => ""
+            ],
+            [
+                "key" => "call_title",
+                "value" => ""
+            ],
+            [
+                "key" => "call_email",
+                "value" => ""
+            ],
+            [
+                "key" => "call_phone",
+                "value" => ""
+            ],
 
-        // $columns = [
-        //     [
-        //         "key" => "header_services_img",
-        //         "value" => ""
-        //     ],
-        //     [
-        //         "key" => "header_services_dis",
-        //         "value" => ""
-        //     ],
-        //     [
-        //         "key" => "header_about_img",
-        //         "value" => ""
-        //     ],
-        //     [
-        //         "key" => "header_about_dis",
-        //         "value" => ""
-        //     ],
-        //     [
-        //         "key" => "sec2_about_img",
-        //         "value" => ""
-        //     ],
-        //     [
-        //         "key" => "sec2_about_dis",
-        //         "value" => ""
-        //     ],
+        ];
+        DB::table('homes')->insert($columns);
 
-        //     [
-        //         "key" => "mission_dis",
-        //         "value" => ""
-        //     ],
+        $columns = [
+            [
+                "key" => "header_services_img",
+                "value" => ""
+            ],
+            [
+                "key" => "header_services_dis",
+                "value" => ""
+            ],
+            [
+                "key" => "header_about_img",
+                "value" => ""
+            ],
+            [
+                "key" => "header_about_dis",
+                "value" => ""
+            ],
+            [
+                "key" => "sec2_about_img",
+                "value" => ""
+            ],
+            [
+                "key" => "sec2_about_dis",
+                "value" => ""
+            ],
 
-        //     [
-        //         "key" => "distinguishes_dis",
-        //         "value" => ""
-        //     ],
-        // ];
+            [
+                "key" => "mission_dis",
+                "value" => ""
+            ],
 
-        // $columns = [
-        //     [
-        //         "key" => "facebook",
-        //         "value" => ""
-        //     ],
-        //     [
-        //         "key" => "youtube",
-        //         "value" => ""
-        //     ],
-        //     [
-        //         "key" => "x",
-        //         "value" => ""
-        //     ],
-        //     [
-        //         "key" => "insta",
-        //         "value" => ""
-        //     ],
+            [
+                "key" => "distinguishes_dis",
+                "value" => ""
+            ],
 
-        // ];
-
-        // $columns = [
-        //     [
-        //         "key" => "header_project_img",
-        //         "value" => ""
-        //     ],
-        //     [
-        //         "key" => "header_project_dis",
-        //         "value" => ""
-        //     ],
-        //     [
-        //         "key" => "project_dis",
-        //         "value" => ""
-        //     ],
-        // ];
-        // $columns = [
-        //     [
-        //         "key" => "distinguishes_img",
-        //         "value" => ""
-        //     ],
-
-        // ];
-
-        // $columns = [
-        //     [
-        //         "key" => "logo",
-        //         "value" => ""
-        //     ],
-        //     [
-        //         "key" => "title",
-        //         "value" => "تكامل"
-        //     ],
-
-        // ];
+            [
+                "key" => "facebook",
+                "value" => ""
+            ],
+            [
+                "key" => "youtube",
+                "value" => ""
+            ],
+            [
+                "key" => "x",
+                "value" => ""
+            ],
+            [
+                "key" => "insta",
+                "value" => ""
+            ],
 
 
-        // $columns = [
-        //     [
-        //         "key" => "header_courses_img",
-        //         "value" => ""
-        //     ],
-        //     [
-        //         "key" => "header_courses_dis",
-        //         "value" => ""
-        //     ],
-        //     [
-        //         "key" => "courses_dis",
-        //         "value" => ""
-        //     ],
-        // ];
+            [
+                "key" => "header_project_img",
+                "value" => ""
+            ],
+            [
+                "key" => "header_project_dis",
+                "value" => ""
+            ],
+            [
+                "key" => "project_dis",
+                "value" => ""
+            ],
 
+            [
+                "key" => "distinguishes_img",
+                "value" => ""
+            ],
+
+
+            [
+                "key" => "logo",
+                "value" => ""
+            ],
+            [
+                "key" => "title",
+                "value" => "تكامل"
+            ],
+
+            [
+                "key" => "header_courses_img",
+                "value" => ""
+            ],
+            [
+                "key" => "header_courses_dis",
+                "value" => ""
+            ],
+            [
+                "key" => "courses_dis",
+                "value" => ""
+            ],
+        ];
 
         DB::table('varibales')->insert($columns);
     }
