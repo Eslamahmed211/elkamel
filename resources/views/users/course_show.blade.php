c351701ab6295bfff60ddcde1134604a13bddd6f
Eslam ahmed <eslamahmedwork@gmail.com>
2024-02-17 02:28:08 +0200

    finsh

--- resources/views/users/course_show.blade.php
+++ resources/views/users/course_show.blade.php
@@ -2,6 +2,8 @@
 
 
 @section('body')
+    <link rel="stylesheet" href="https://souqelgom3a.online/assets/admin/css/apexcharts.css">
+
     <style>
         .swiper {
             width: 90%;
@@ -56,6 +58,50 @@
         }
     </style>
 
+    <style>
+        .precent {
+            display: flex;
+            align-items: center;
+            position: relative;
+            justify-content: center;
+        }
+
+        .parant {
+            display: flex;
+            align-items: center;
+        }
+
+        @media (max-width:993px) {
+            .parant {
+                flex-direction: column;
+            }
+        }
+
+        .precent .count {
+            position: absolute;
+            pointer-events: none;
+            width: 102px;
+            transform: translate(-50%, -50%);
+            /* overflow: hidden; */
+            white-space: nowrap;
+            text-overflow: ellipsis;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-shadow: 0px 0px 0px;
+            -webkit-text-stroke: 0px;
+            font-weight: bold;
+            color: rgba(44, 53, 66, 0.85) !important;
+            text-align: center;
+            font-size: 25px;
+            line-height: 1;
+            left: 50%;
+            top: 50%;
+            z-index: 7;
+            font-weight: 500;
+        }
+    </style>
+
 
     <!-- start landing -->
     <div class="bg-[url(../images/headerBackgrond.png)] bg-no-repeat bg-cover">
@@ -69,12 +115,27 @@
                 </div>
 
                 <div class="basis-[60%]">
-                    <p class="text-[24px] font-[700] text-[#042E6F] mb-2">{{ $course->title }}</p>
 
-                    <p class="text-[20px] font-[700] text-[#042E6F] mb-2">{{ $course->start_at }} <span
-                            style="margin-right: 15px">{{ $course->end_at }} </span> </p>
+                    <div class="parant">
 
-                    <p class="text-[18px] font-[700] text-[#042E6F] mb-2">نسبة الاكتمال % {{ $course->percent }} </p>
+                        <div>
+                            <p class="text-[24px] font-[700] text-[#042E6F] mb-2">{{ $course->title }}</p>
+
+                            <p class="text-[20px] font-[700] text-[#042E6F] m-2">{{ $course->start_at }} <span
+                                    style="margin-right: 15px">{{ $course->end_at }} </span> </p>
+
+                        </div>
+
+                        <div class="precent_contaner">
+                            <div class="precent" style="width: 200px;height: 200px">
+                                <div id="precentChart">
+                                    <div class="count"> {{ $course->percent }} %</div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
 
 
                     <p class="text-[20px] font-[400] text-[#fff] max-w-[558px]">
@@ -122,7 +183,14 @@
 
     </div>
 
-    <script src="https://zamzamfoods-eg.com/assets/sw/swiper-element.js"></script>
+    {{-- <script src="https://zamzamfoods-eg.com/assets/sw/swiper-element.js"></script> --}}
+
+    <script src="/js/swiper.js"></script>
+
+
+
+    <script src="https://souqelgom3a.online/assets/admin/js/apexcharts.min.js"></script>
+
 
     <script>
         var swiper = new Swiper(".mySwiper", {
@@ -159,5 +227,50 @@
             },
 
         });
+    </script>
+
+    <script>
+        var options = {
+            series: [{{ 100 - $course->percent }}, {{ $course->percent }}],
+            labels: ["الغير مكتملة", "نسبة الاكتمال"],
+            dataLabels: {
+                enabled: false,
+                total: {
+                    show: true // Show the total value
+                }
+            },
+            plotOptions: {
+                pie: {
+                    expandOnClick: false,
+                    donut: {
+                        size: '75%',
+                    },
+                },
+            },
+            chart: {
+                type: 'donut',
+            },
+            colors: ['#fa776e', '#0d9daa'],
+            tooltip: {
+                enabled: true,
+                fillSeriesColor: false,
+                style: {
+                    fontSize: '12px',
+                    fontFamily: 'cairo',
+                    fontWeight: '500',
+                    color: 'rgb(89, 89, 89)',
+                    background: 'red',
+                    border: '1px solid #555',
+                }
+            },
+            legend: {
+                show: false,
+            },
+        }
+
+
+
+        var chart = new ApexCharts(document.querySelector("#precentChart"), options);
+        chart.render();
     </script>
 @endsection
--- resources/views/users/layout.blade.php
+++ resources/views/users/layout.blade.php
@@ -58,6 +58,10 @@
             },
         };
     </script>
+
+
+
+
 </head>
 
 <body>
